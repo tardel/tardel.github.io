a39998972c4aaf472c95c958776e512df7827761
Bryson Rubio <92761091+tardel@users.noreply.github.com>
2021-10-20 12:01:09 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var a = [];
 function yo(){
   var thing = document.getElementById('name').value;
   document.getElementById('name').value ="";
-  a.push(thing)
-  window.alert(a);
+  window.alert(thing);
 }
 
