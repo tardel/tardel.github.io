5b2182317049b4305ab149ff7a7bf37675e918e5
Bryson Rubio <92761091+tardel@users.noreply.github.com>
2021-10-22 11:29:12 -1000

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function yo(){
  function call(){
   window.alert(thing)
  }
+
+var a;
+
+function getName(){
+  //get value from input field and save as "a"
+  a = document.getElementById('username').value;
+  //create LS key with value of "a"
+  localStorage.setItem('uName',a);
+  //automatically take user to page two.html
+  location.href="two.html";
+}
+
+function getUser(){
+  var b = localStorage.getItem('uName');
+  document.getElementById('placeholder').innerHTML = "Hello "+b;
+}
